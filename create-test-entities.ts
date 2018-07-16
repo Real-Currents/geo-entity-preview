4cb9a3d62ecd25d995049daa8ec31f9c09b0a086
John <john@real-currents.com>
2018-07-16 19:52:00 -0400

    get some entities

--- create-test-entities.ts
+++ create-test-entities.ts
@@ -1,6 +1,8 @@
-import {csvParse, tsvParse} from 'd3-dsv';
+import { tsvParse } from 'd3-dsv';
 
-export default function creatTestEntities (fromCSV: string, parseFn?) {
+export default async function createTestEntities (fromCSV: string, parseFn?) {
+
+    let promise: Promise<any>;
 
     const getData = async () => {
         console.log('Fetching data');
@@ -11,41 +13,54 @@ export default function creatTestEntities (fromCSV: string, parseFn?) {
         };
     };
 
-    (async function() {
-        let someData;
 
-        console.log(typeof parseFn);
+    // const dataPromise = (async function() {
+        console.log('Fetching data');
 
-        if (typeof parseFn == 'function') {
-            someData = tsvParse((await getData()).responseText);
-        } else {
-            someData = tsvParse((await getData()).responseText, parseFn);
-        }
+        promise = (await fetch(`./data/${fromCSV}`)).text();
 
-        console.log(someData);
+        return promise;
+    // })();
+    //
+    // dataPromise.then(
+    //     response => {
+    //         console.log('Async Work Complete: ');
+    //
+    //         const someData = tsvParse(response);
+    //
+    //         console.log(typeof parseFn);
+    //
+    //         if (typeof parseFn == 'function') {
+    //             someData.map(parseFn);
+    //         }
+    //     },
+    //     error => {
+    //         console.log(error);
+    //     }
+    // );
+    //
+    // return dataPromise;
 
-        // const regions = someData.reduce((last: [ any ], row) => {
-        //     const region = row.Region;
-        //     if (!last[ region ]) {
-        //         last[ region ] = [];
-        //     }
-        //     last[ region ].push(row);
-        //     return last;
-        // }, {});
-        //
-        // console.log(regions);
-        //
-        // const regionsPctTurnout = Object.entries(regions)
-        //     .map(([ region, areas ]) => {
-        //             return {
-        //                 Region: region,
-        //                 'First Region Code': areas[0]['Region_Code'],
-        //                 'Mean % Turnout': d3.mean(areas as ArrayLike<any>, d => d[ 'Pct_Turnout' ])
-        //             };
-        //         }
-        //     );
-        //
-        // console.log(regionsPctTurnout);
-
-    })();
+    // const regions = someData.reduce((last: [ any ], row) => {
+    //     const region = row.Region;
+    //     if (!last[ region ]) {
+    //         last[ region ] = [];
+    //     }
+    //     last[ region ].push(row);
+    //     return last;
+    // }, {});
+    //
+    // console.log(regions);
+    //
+    // const regionsPctTurnout = Object.entries(regions)
+    //     .map(([ region, areas ]) => {
+    //             return {
+    //                 Region: region,
+    //                 'First Region Code': areas[0]['Region_Code'],
+    //                 'Mean % Turnout': d3.mean(areas as ArrayLike<any>, d => d[ 'Pct_Turnout' ])
+    //             };
+    //         }
+    //     );
+    //
+    // console.log(regionsPctTurnout);
 }
--- main.ts
+++ main.ts
@@ -1,5 +1,69 @@
 import createTestEntities from './create-test-entities';
+import {async} from "rxjs/scheduler/async";
+import {tsvParse} from "d3-dsv";
 
-// const style = require('./styles/index.css');
+const style = require('./styles/index.css');
 
-createTestEntities('AL071_with_header.tsv');
+const output = document
+    .getElementsByTagName('body')[0]
+    .appendChild(document.createElement('pre'));
+
+(async () => await createTestEntities('AL071_with_header.tsv')
+    .then(response => tsvParse(response).map(d => {
+        const entity: {
+            id: string,
+            entityType: string,
+            geometry: any,
+            properties: any
+        } = {
+            'id': '',
+            'entityType': '',
+            'geometry': '',
+            'properties': {}
+        };
+
+        for (const p in d) {
+            switch (p) {
+                case 'id':
+                    entity['id'] = d[p];
+                    break;
+                case 'entitytype':
+                    entity['entityType'] = d[p];
+                    entity['entityType'] = '';
+                    break;
+                case 'index_geometry':
+                    try {
+                        entity['geometry'] = JSON.parse(d[p].toString());
+                    } catch (e) {
+                        console.log(`GeoJSON parse error: ${e}`);
+                        entity['geometry'] = d[p];
+                    }
+                    break;
+                case 'a2_code':
+                    entity['properties']['A2_CODE'] = d[p];
+                    break;
+                case 'micode':
+                    entity['properties']['MICODE'] = d[p];
+                    break;
+                default:
+                    const prop = p.toString()
+                        .replace(/^\w/,(chr) => chr.toUpperCase())
+                        .replace(/\_(\w)/,(chr, first) => ' '+ first.toUpperCase())
+                        .replace(/\_/, ' ');
+                    entity['properties'][prop] = d[p];
+            }
+        }
+
+        console.log(entity);
+
+        output.innerHTML = output.innerHTML +`
+${JSON.stringify(entity)},
+`;
+
+        return d;
+    }),
+    error => {
+        console.log(error);
+    })
+)();
+
